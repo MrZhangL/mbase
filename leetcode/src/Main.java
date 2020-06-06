2f2de1f2c13000e7ea4e836e17c8f1797f882cc3
zhanglei <a535665407@qq.com>
2020-06-06 16:31:52 +0800

    add Main.java

---
+++ leetcode/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
+        list.clear();
+        System.out.println(set.remove(1));
+        Iterator<Integer> it = set.iterator();
+
+    }
+}
